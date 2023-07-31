75d4105704b48decc1dc95d77bde2293188fc3b7
dubiousconst282 <87553666+dubiousconst282@users.noreply.github.com>
2023-07-31 19:17:48 -0300

    Normal mapping

--- src/SwRast/Main.cpp
+++ src/SwRast/Main.cpp
@@ -43,15 +43,41 @@ struct PhongShader {
 
         swr::VFloat u = vars.GetSmooth(0);
         swr::VFloat v = vars.GetSmooth(1);
-        swr::VFloat4 diffuseColor = DiffuseTex->SampleNearest(u, v);
+        swr::VFloat4 diffuseColor = DiffuseTex->SampleHybrid(u, v);
 
-        swr::VFloat3 norm = swr::simd::normalize({ vars.GetSmooth(2), vars.GetSmooth(3), vars.GetSmooth(4) });
+        swr::VFloat3 N = swr::simd::normalize({ vars.GetSmooth(2), vars.GetSmooth(3), vars.GetSmooth(4) });
+
+        if (NormalTex != nullptr) {
+            // T = normalize(T - dot(T, N) * N);
+            // mat3 TBN = mat3(T, cross(N, T), N);
+            // vec3 n = texture(u_NormalTex, UV).rgb * 2.0 - 1.0;
+            // norm = normalize(TBN * n);
+            swr::VFloat3 T = swr::simd::normalize({ vars.GetSmooth(5), vars.GetSmooth(6), vars.GetSmooth(7) });
+            
+            // Gram-schmidt process (produces higher-quality normal mapping on large meshes)
+            // Re-orthogonalize T with respect to N
+            swr::VFloat TN = swr::simd::dot(T, N);
+            T = swr::simd::normalize({ T.x - TN * N.x, T.y - TN * N.y, T.z - TN * N.z });
+            swr::VFloat3 B = swr::simd::cross(N, T);
+
+            swr::VFloat4 SN = NormalTex->SampleHybrid(u, v);
+
+            // https://aras-p.info/texts/CompactNormalStorage.html#q-comparison
+            swr::VFloat Sx = SN.x * 2.0f - 1.0f;
+            swr::VFloat Sy = SN.y * 2.0f - 1.0f;
+            swr::VFloat Sz = swr::simd::sqrt14(1.0f - Sx * Sx + Sy * Sy);  // sqrt(1.0f - dot(n.xy, n.xy));
+
+            N.x = T.x * Sx + B.x * Sy + N.x * Sz;
+            N.y = T.y * Sx + B.y * Sy + N.y * Sz;
+            N.z = T.z * Sx + B.z * Sy + N.z * Sz;
+        }
+
         swr::VFloat3 lightDir = swr::simd::normalize({
             LightPos.x - vars.GetSmooth(6),
             LightPos.y - vars.GetSmooth(7),
             LightPos.z - vars.GetSmooth(8),
         });
-        swr::VFloat NdotL = swr::simd::max(swr::simd::dot(norm, lightDir), 0.0f);
+        swr::VFloat NdotL = swr::simd::max(swr::simd::dot(N, lightDir), 0.0f);
 
         swr::VFloat4 color = {
             diffuseColor.x * NdotL,
@@ -61,7 +87,7 @@ struct PhongShader {
         };
 
         auto rgba = swr::simd::PackRGBA(color);
-        uint16_t alphaMask = _mm512_cmpgt_epu32_mask(rgba, _mm512_set1_epi32(128 << 24));
+        uint16_t alphaMask = _mm512_cmpgt_epu32_mask(rgba, _mm512_set1_epi32(200 << 24));
         fb.WriteTile(vars.TileOffset, vars.TileMask & alphaMask, rgba, vars.Depth);
     }
 };
@@ -77,14 +103,18 @@ class SwRenderer {
 
 public:
     SwRenderer() {
-        _fb = std::make_shared<swr::Framebuffer>(1920, 1080);
-        _rast = std::make_unique<swr::Rasterizer>(_fb);
-
         _model = std::make_unique<Model>("Models/Sponza/Sponza.gltf");
         //_model = std::make_unique<Model>("Models/sea_keep_lonely_watcher/scene.gltf");
         //_model = std::make_unique<Model>("Models/San_Miguel/san-miguel-low-poly.obj");
         _cam = Camera();
-        _cam.MoveSpeed = 15.0f;
+        _cam.MoveSpeed = 10.0f;
+
+        InitRasterizer(1920, 1080);
+    }
+
+    void InitRasterizer(uint32_t width, uint32_t height) {
+        _fb = std::make_shared<swr::Framebuffer>(width, height);
+        _rast = std::make_unique<swr::Rasterizer>(_fb);
 
         _glTex = std::make_unique<ogl::Texture2D>(_fb->Width, _fb->Height, 1, GL_RGBA8);
         _tempPixels = std::make_unique<uint32_t[]>(_fb->Width * _fb->Height);
@@ -92,6 +122,27 @@ public:
 
     void Render() {
         auto renderStart = std::chrono::high_resolution_clock::now();
+
+        static bool s_NormalMapping = true;
+
+        ImGui::Begin("Settings");
+
+        std::string currRes = std::format("{}x{}", _fb->Width, _fb->Height);
+        if (ImGui::BeginCombo("Res", currRes.c_str())) {
+            if (ImGui::Selectable("1920x1080")) InitRasterizer(1920, 1080);
+            if (ImGui::Selectable("1280x720")) InitRasterizer(1280, 720);
+            if (ImGui::Selectable("854x480")) InitRasterizer(854, 480);
+            if (ImGui::Selectable("320x240")) InitRasterizer(320, 240);
+            ImGui::EndCombo();
+        }
+
+        ImGui::Checkbox("Normal Mapping", &s_NormalMapping);
+        ImGui::Checkbox("Wireframe", &_rast->EnableWireframe);
+
+        static int s_MaxDrawProgress = _model->Meshes.size();
+        ImGui::SliderInt("Draw Progress", &s_MaxDrawProgress, 1, _model->Meshes.size());
+        ImGui::SliderFloat("CamSpeed", &_cam.MoveSpeed, 0.5f, 500.0f, "%.1f", ImGuiSliderFlags_Logarithmic);
+        ImGui::End();
 
         _cam.Update();
 
@@ -101,14 +152,15 @@ public:
         };
 
         _fb->Clear(0xD0EEFF, 1.0f);
-        
+
         for (Mesh& mesh : _model->Meshes) {
             shader.DiffuseTex = mesh.Material->DiffuseTex;
+            shader.NormalTex = s_NormalMapping ? mesh.Material->NormalTex : nullptr;
 
             swr::VertexReader data(
                 (uint8_t*)&_model->VertexBuffer[mesh.VertexOffset], 
                 (uint8_t*)&_model->IndexBuffer[mesh.IndexOffset],
-                mesh.IndexCount, swr::VertexReader::U32);
+                mesh.IndexCount, swr::VertexReader::U16);
 
             _rast->DrawIndexed(data, shader);
         }
@@ -129,12 +181,11 @@ public:
         drawList->AddImage(texId, drawList->GetClipRectMin(), drawList->GetClipRectMax(), ImVec2(0, 1), ImVec2(1, 0));
 
         ImGui::Begin("Rasterizer Stats");
-        ImGui::Text("Frame: %.2fms (%.0f FPS) @ %dx%d", totalElapsed, 1000.0 / totalElapsed, _fb->Width, _fb->Height);
+        ImGui::Text("Frame: %.2fms (%.0f FPS)", totalElapsed, 1000.0 / totalElapsed);
         ImGui::Text("Vertex setup: %.2fms", stats.VertexSetup[0] / 1000000.0);
         ImGui::Text("Rasterize: %.2fms", stats.Rasterize[0] / 1000000.0);
         ImGui::Text("Triangles: %.1fK (%.1fK clipped)", stats.TrianglesDrawn / 1000.0, stats.TrianglesClipped / 1000.0);
         ImGui::Text("Bins filled: %.1fK", stats.BinsFilled / 1000.0);
-        ImGui::Checkbox("Wireframe", &_rast->EnableWireframe);
         stats.Reset();
         ImGui::End();
 
